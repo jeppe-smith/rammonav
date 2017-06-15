dc064dfbe7c6b60bb021412ad0a940fcae477b45
Jeppe Smith <jws@ecreo.dk>
2017-06-15 14:23:48 +0200

    make it umd compatible

--- index.js
+++ index.js
@@ -1,162 +1,196 @@
-'use strict';
+(function (global, factory) {
+  if (typeof define === "function" && define.amd) {
+    define(['exports'], factory);
+  } else if (typeof exports !== "undefined") {
+    factory(exports);
+  } else {
+    var mod = {
+      exports: {}
+    };
+    factory(mod.exports);
+    global.rammonav = mod.exports;
+  }
+})(this, function (exports) {
+  'use strict';
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-window.requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame || function (f) {
-  return setTimeout(f, 1000 / 60);
-};
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+  exports.default = Rammonav;
+  window.requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame || function (f) {
+    return setTimeout(f, 1000 / 60);
+  };
 
-window.cancelAnimationFrame = window.cancelAnimationFrame || window.mozCancelAnimationFrame || function (requestID) {
-  clearTimeout(requestID);
-};
+  window.cancelAnimationFrame = window.cancelAnimationFrame || window.mozCancelAnimationFrame || function (requestID) {
+    clearTimeout(requestID);
+  };
 
-var rammonav = {
-  cache: function cache(container, sub) {
-    var _this = this;
+  var rammonav = {
+    cache: function cache(container, sub) {
+      var _this = this;
 
-    this.container = container;
-    this.nav = this.container.children[0];
-    this.links = Array.from(this.nav.children);
+      this.container = container;
+      this.nav = this.container.children[0];
+      this.links = Array.from(this.nav.children);
 
-    this.links.forEach(function (link, index) {
-      return link.setAttribute('rammo-id', index);
-    });
-
-    this.sub = sub;
-    this.subNav = this.nav.cloneNode(true);
-    this.subLinks = Array.from(this.subNav.children);
-
-    this.links.forEach(function (link, index) {
-      return link.setAttribute('rammo-width', link.clientWidth);
-    });
-
-    requestAnimationFrame(function () {
-      _this.subLinks.forEach(function (link) {
-        link.classList.add('rammo-sublink');
-        link.style.display = 'none';
+      this.links.forEach(function (link, index) {
+        return link.setAttribute('rammo-id', index);
       });
 
-      _this.sub.appendChild(_this.subNav);
-    });
-  },
-  bind: function bind() {
-    window.addEventListener('resize', this.handleResize.bind(this));
-  },
-  handleResize: function handleResize() {
-    var maxWidth = this.container.clientWidth;
-    var width = this.nav.clientWidth;
+      this.sub = sub;
+      this.subNav = this.nav.cloneNode(true);
+      this.subLinks = Array.from(this.subNav.children);
 
-    if (width > maxWidth) {
-      requestAnimationFrame(this.removeLastLinkFromNav.bind(this));
-    } else {
-      requestAnimationFrame(this.addNextLinkToNav.bind(this));
-    }
-  },
-  isHidden: function isHidden(element) {
-    return getComputedStyle(element).display === 'none';
-  },
-  hideElement: function hideElement(element) {
-    return element.style.display = 'none';
-  },
-  showElement: function showElement(element) {
-    return element.style.display = '';
-  },
-  removeLastLinkFromNav: function removeLastLinkFromNav() {
-    var _this2 = this;
+      this.links.forEach(function (link, index) {
+        return link.setAttribute('rammo-width', link.clientWidth);
+      });
 
-    var maxWidth = this.container.clientWidth;
-    var width = this.nav.clientWidth;
-    var lastIndex = this.links.length - 1;
-    var lastLink = this.links[lastIndex];
-    var lastLinkId = lastLink.getAttribute('rammo-id');
-    var isHidden = this.isHidden(lastLink);
+      requestAnimationFrame(function () {
+        _this.subLinks.forEach(function (link) {
+          link.classList.add('rammo-sublink');
+          link.style.display = 'none';
+        });
 
-    while (isHidden) {
-      lastIndex = lastIndex - 1;
-      lastLink = this.links[lastIndex];
-      lastLinkId = lastLink.getAttribute('rammo-id');
-      isHidden = this.isHidden(lastLink);
+        _this.sub.appendChild(_this.subNav);
+      });
+    },
+    bind: function bind() {
+      window.addEventListener('resize', this.handleResize.bind(this));
+    },
+    handleResize: function handleResize() {
+      var maxWidth = this.container.clientWidth;
+      var width = this.nav.clientWidth;
 
-      if (lastIndex === 0) {
-        break;
+      if (width > maxWidth) {
+        requestAnimationFrame(this.removeLastLinkFromNav.bind(this));
+      } else {
+        requestAnimationFrame(this.addNextLinkToNav.bind(this));
       }
-    }
+    },
+    isHidden: function isHidden(element) {
+      return getComputedStyle(element).display === 'none';
+    },
+    hideElement: function hideElement(element) {
+      return element.style.display = 'none';
+    },
+    showElement: function showElement(element) {
+      return element.style.display = '';
+    },
+    toggleSubClass: function toggleSubClass() {
+      var _this2 = this;
 
-    this.hideElement(lastLink);
+      var hasVisibleLinks = this.subLinks.filter(function (link) {
+        return !_this2.isHidden(link);
+      }).length;
 
-    this.subLinks.forEach(function (link) {
-      var linkId = link.getAttribute('rammo-id');
-
-      if (linkId === lastLinkId) {
-        _this2.showElement(link);
+      if (hasVisibleLinks) {
+        this.sub.classList.remove('empty');
+      } else {
+        this.sub.classList.add('empty');
       }
-    });
+    },
+    removeLastLinkFromNav: function removeLastLinkFromNav() {
+      var _this3 = this;
 
-    width = this.nav.clientWidth;
+      var maxWidth = this.container.clientWidth;
+      var width = this.nav.clientWidth;
+      var lastIndex = this.links.length - 1;
+      var lastLink = this.links[lastIndex];
+      var lastLinkId = lastLink.getAttribute('rammo-id');
+      var isHidden = this.isHidden(lastLink);
 
-    if (width > maxWidth) {
-      return requestAnimationFrame(this.removeLastLinkFromNav.bind(this));
-    } else {
-      return true;
-    }
-  },
-  addNextLinkToNav: function addNextLinkToNav() {
-    var _this3 = this;
+      while (isHidden) {
+        lastIndex = lastIndex - 1;
+        lastLink = this.links[lastIndex];
+        lastLinkId = lastLink.getAttribute('rammo-id');
+        isHidden = this.isHidden(lastLink);
 
-    var maxWidth = this.container.clientWidth;
-    var lastLink = this.links[this.links.length - 1];
-    var lastLinkIsVisible = !this.isHidden(lastLink);
-    var width = this.nav.clientWidth;
-    var nextIndex = 0;
-    var nextLink = this.links[nextIndex];
-    var nextLinkId = nextLink.getAttribute('rammo-id');
-    var nextLinkWidth = parseInt(nextLink.getAttribute('rammo-width'));
-    var isVisible = !this.isHidden(nextLink);
-
-    if (lastLinkIsVisible) {
-      return true;
-    }
-
-    while (isVisible) {
-      nextIndex = nextIndex + 1;
-      nextLink = this.links[nextIndex];
-      nextLinkId = nextLink.getAttribute('rammo-id');
-      nextLinkWidth = parseInt(nextLink.getAttribute('rammo-width'));
-      isVisible = !this.isHidden(nextLink);
-
-      if (nextIndex >= this.links.length - 1) {
-        break;
+        if (lastIndex === 0) {
+          break;
+        }
       }
-    }
 
-    if (width + nextLinkWidth > maxWidth) {
-      return true;
-    }
+      this.hideElement(lastLink);
 
-    this.showElement(nextLink);
+      this.subLinks.forEach(function (link) {
+        var linkId = link.getAttribute('rammo-id');
 
-    this.subLinks.forEach(function (link) {
-      var linkId = link.getAttribute('rammo-id');
+        if (linkId === lastLinkId) {
+          _this3.showElement(link);
+        }
+      });
 
-      if (linkId === nextLinkId) {
-        _this3.hideElement(link);
+      width = this.nav.clientWidth;
+
+      this.toggleSubClass();
+
+      if (width > maxWidth) {
+        return requestAnimationFrame(this.removeLastLinkFromNav.bind(this));
+      } else {
+        return true;
       }
-    });
+    },
+    addNextLinkToNav: function addNextLinkToNav() {
+      var _this4 = this;
 
-    width = this.nav.clientWidth;
+      var maxWidth = this.container.clientWidth;
+      var lastLink = this.links[this.links.length - 1];
+      var lastLinkIsVisible = !this.isHidden(lastLink);
+      var width = this.nav.clientWidth;
+      var nextIndex = 0;
+      var nextLink = this.links[nextIndex];
+      var nextLinkId = nextLink.getAttribute('rammo-id');
+      var nextLinkWidth = parseInt(nextLink.getAttribute('rammo-width'));
+      var isVisible = !this.isHidden(nextLink);
 
-    if (width < maxWidth) {
-      return requestAnimationFrame(this.addNextLinkToNav.bind(this));
-    } else {
-      return true;
+      if (lastLinkIsVisible) {
+        return true;
+      }
+
+      while (isVisible) {
+        nextIndex = nextIndex + 1;
+        nextLink = this.links[nextIndex];
+        nextLinkId = nextLink.getAttribute('rammo-id');
+        nextLinkWidth = parseInt(nextLink.getAttribute('rammo-width'));
+        isVisible = !this.isHidden(nextLink);
+
+        if (nextIndex >= this.links.length - 1) {
+          break;
+        }
+      }
+
+      if (width + nextLinkWidth > maxWidth) {
+        return true;
+      }
+
+      this.showElement(nextLink);
+
+      this.subLinks.forEach(function (link) {
+        var linkId = link.getAttribute('rammo-id');
+
+        if (linkId === nextLinkId) {
+          _this4.hideElement(link);
+        }
+      });
+
+      width = this.nav.clientWidth;
+
+      this.toggleSubClass();
+
+      if (width < maxWidth) {
+        return requestAnimationFrame(this.addNextLinkToNav.bind(this));
+      } else {
+        return true;
+      }
+    },
+    init: function init(container, sub) {
+      this.cache(container, sub);
+      this.bind();
+      this.handleResize();
     }
-  },
-  init: function init(container, sub) {
-    this.cache(container, sub);
-    this.bind();
-    this.handleResize();
+  };
+
+  function Rammonav(nav, subnav) {
+    return rammonav.init(nav, subnav);
   }
-};
-
-exports.default = rammonav.init.bind(rammonav);
+});
